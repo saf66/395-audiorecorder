5d6f9f7c17daaab2bfade57cae03747ebbdddd47
Sean <saf66@case.edu>
2013-03-19 07:21:55 -0400

    Fixed the threading model

--- AudioRecord/src/com/example/audiorecord/MainActivity.java
+++ AudioRecord/src/com/example/audiorecord/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.audiorecord;
 
 import java.io.FileOutputStream;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.zip.GZIPOutputStream;
 
 import android.media.AudioFormat;
@@ -21,6 +22,8 @@ import android.app.Activity;
 public class MainActivity extends Activity implements OnClickListener {
 	
 	private AudioInputThread recordingThread = null;
+	private ConsumerThread processingThread = null;
+	private LinkedBlockingQueue<Short> sharedQueue = new LinkedBlockingQueue<Short>();
 	
 	private Button btnToggle = null;
 	private TextView txtDisplay = null;
@@ -40,17 +43,86 @@ public class MainActivity extends Activity implements OnClickListener {
 	public void onClick(View v) {
 		switch (v.getId()) {
 			case R.id.btnToggle: {
-				if (recordingThread != null && recordingThread.isAlive()) {
+				if (recordingThread != null && recordingThread.isAlive() && processingThread != null && processingThread.isAlive()) {
 	            	recordingThread.close();
 	            	recordingThread = null;
+	            	processingThread.close();
+	            	processingThread = null;
 	            	((Button) v).setText(getString(R.string.start));
 	            } else {
 	            	((Button) v).setText(getString(R.string.stop));
+	            	processingThread = new ConsumerThread();
+	            	processingThread.start();
 	            	recordingThread = new AudioInputThread();
 	            	recordingThread.start();
 	            }
 			}
 		}
+	}
+	
+	//[TODO] This is just an example of how to read from the shared queue. Actual processing code would go here.
+	private class ConsumerThread extends Thread {
+
+		private static final String TAG = "ConsumerThread";
+		
+		private boolean running = false;
+		
+		private ConsumerThread() {
+			setDaemon(true);
+		}
+
+		@Override
+		public void run() {
+			GZIPOutputStream zos = null;
+			try {
+				zos = new GZIPOutputStream(new FileOutputStream(Environment.getExternalStorageDirectory().getAbsolutePath() + "/audio.pcm.gz"));
+				running = true;
+				while (running) {
+					byte[] data = new byte[256];
+					for (int i = 0; i < data.length; i++) {
+						// LinkedBlockingQueue.take() is a blocking operation
+						data[i] = requantize(sharedQueue.take().shortValue());
+					}
+					zos.write(data, 0, data.length);
+				}
+				zos.flush();
+				zos.finish();
+				zos.close();
+			} catch (Throwable e) {
+				e.printStackTrace();
+			} finally {
+				zos = null;
+			}
+			Log.d(TAG, "thread finished");
+		}
+		
+		private byte requantize(short data) {
+			byte out = (byte) (data >> 8);
+			if (out < 0xff && ((data & 0xff) > 0x80)) {
+				out++;
+			}
+			return out;
+		}
+	    
+		/*
+	    private byte[] short2byte(short[] input, int iterations) {
+			int short_index, byte_index;
+			byte[] buffer = new byte[iterations * 2];
+			short_index = byte_index = 0;
+			while (short_index != iterations) {
+			    buffer[byte_index]     = (byte) (input[short_index] & 0x00FF); 
+			    buffer[byte_index + 1] = (byte) ((input[short_index] & 0xFF00) >> 8);
+			    ++short_index;
+			    byte_index += 2;
+			}
+			return buffer;
+	    }
+	    */
+		
+		private void close() {
+			running = false;
+		}
+		
 	}
 	
 	private class AudioInputThread extends Thread {
@@ -73,59 +145,27 @@ public class MainActivity extends Activity implements OnClickListener {
 		public void run() {
 			Log.d(TAG, "Recording thread started");
 			Process.setThreadPriority(Process.THREAD_PRIORITY_URGENT_AUDIO);
-			GZIPOutputStream zos = null;
 			AudioRecord recorder = null;
 			try {
 				short[] buffer = new short[RECORDER_MIN_BUFFER];
-				zos = new GZIPOutputStream(new FileOutputStream(Environment.getExternalStorageDirectory().getAbsolutePath() + "/audio.pcm.gz"));
 				recorder = new AudioRecord(RECORDER_SOURCE, RECORDER_SAMPLERATE, RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING, buffer.length);
 				recorder.startRecording();
 				running = true;
 				while (running) {
 					int samples = recorder.read(buffer, 0, buffer.length);
-					//[TODO] push the buffer into a queue for another thread to process
-					byte[] data = requantize(buffer, samples);
-					zos.write(data, 0, data.length);
+					for (int i = 0; i < samples; i++) {
+						sharedQueue.offer((Short) buffer[i]);
+					}
 				}
-				zos.flush();
-				zos.finish();
-				zos.close();
 			} catch (Throwable e) {
 				e.printStackTrace();
 			} finally {
 				recorder.stop();
 				recorder.release();
 				recorder = null;
-				zos = null;
 			}
-			Log.d(TAG, "Recording thread finished");
+			Log.d(TAG, "thread finished");
 		}
-		
-		private byte[] requantize(short[] data, int size) {
-			byte[] out = new byte[size];
-			for (int i = 0; i < out.length; i++) {
-				out[i] = (byte) (data[i] >> 8);
-				if (out[i] < 0xff && ((data[i] & 0xff) > 0x80)) {
-					out[i]++;
-				}
-			}
-			return out;
-		}
-	    
-		/*
-	    private byte[] short2byte(short[] input, int iterations) {
-			int short_index, byte_index;
-			byte[] buffer = new byte[iterations * 2];
-			short_index = byte_index = 0;
-			while (short_index != iterations) {
-			    buffer[byte_index]     = (byte) (input[short_index] & 0x00FF); 
-			    buffer[byte_index + 1] = (byte) ((input[short_index] & 0xFF00) >> 8);
-			    ++short_index;
-			    byte_index += 2;
-			}
-			return buffer;
-	    }
-	    */
 		
 		private void close() {
 			running = false;
